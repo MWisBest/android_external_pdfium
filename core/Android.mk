1e035f826ac66d5aaf81ea20b8c2afccc97c4c1e
Kyle Repinski <repinski23@gmail.com>
2014-12-07 23:21:14 -0600

    core + fpdfsdk: Disable -fstrict-aliasing unless told otherwise.
    Before forcing -fstrict-aliasing, you should fix strict aliasing violations...

--- core/Android.mk
+++ core/Android.mk
@@ -8,8 +8,12 @@ LOCAL_ARM_MODE := arm
 LOCAL_SDK_VERSION := 19
 LOCAL_NDK_STL_VARIANT := gnustl_static
 
-LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays -fexceptions
+LOCAL_CFLAGS += -O3 -fprefetch-loop-arrays -fexceptions
 LOCAL_CFLAGS += -Wno-non-virtual-dtor -Wall
+
+ifneq ($(DEBUG_FORCE_STRICT_ALIASING),true)
+	LOCAL_CFLAGS += -fno-strict-aliasing
+endif
 
 # Work around gcc text relocation bug. Fixed in gcc 4.9.
 # TODO: remove this line after we've upgraded to gcc 4.9.
--- fpdfsdk/Android.mk
+++ fpdfsdk/Android.mk
@@ -8,9 +8,13 @@ LOCAL_ARM_MODE := arm
 LOCAL_SDK_VERSION := 19
 LOCAL_NDK_STL_VARIANT := gnustl_static
 
-LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays -fexceptions
+LOCAL_CFLAGS += -O3 -fprefetch-loop-arrays -fexceptions
 LOCAL_CFLAGS += -Wno-non-virtual-dtor -Wall
 LOCAL_CFLAGS += -DFOXIT_CHROME_BUILD
+
+ifneq ($(DEBUG_FORCE_STRICT_ALIASING),true)
+	LOCAL_CFLAGS += -fno-strict-aliasing
+endif
 
 LOCAL_STATIC_LIBRARIES := libpdfiumcore
 
